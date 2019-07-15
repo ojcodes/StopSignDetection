b4750f5dc5aeb5f81027acb49e3a50c4d9776db1
Osama Javaid <31596193+ojcodes@users.noreply.github.com>
2019-07-14 21:43:10 -0400

    Add files via upload

--- v9_StopSign_video.py
+++ v9_StopSign_video.py
@@ -1,7 +1,6 @@
 #This script detects stop signs based on haar cascade on videos
 import time
 import cv2 as cv
-import matplotlib.pyplot as plt
 import zmq
 import numpy as np
 import pygame
@@ -10,20 +9,34 @@ from selfdrive.services import service_list
 from common.transformations.camera import FULL_FRAME_SIZE
 start = time.time()
 
+size = (640, 480)
+#_BB_OFFSET = 320,180
+_BB_OFFSET = 0,0
+_BB_SCALE = 1164/640.
+
+_BB_TO_FULL_FRAME = np.asarray([
+    [_BB_SCALE, 0., _BB_OFFSET[0]],
+    [0., _BB_SCALE, _BB_OFFSET[1]],
+    [0., 0.,   1.]])
+
+#pygame.display.set_caption("Vision Stopsigns")
+screen = pygame.display.set_mode(size, pygame.DOUBLEBUF)
+
+camera_surface = pygame.surface.Surface((640, 480), 0, 24).convert()
+#camera_surface = pygame.surface.Surface((1164, 640), 0, 24).convert() #oj
+imgff = np.zeros((FULL_FRAME_SIZE[1], FULL_FRAME_SIZE[0], 3), dtype=np.uint8)
+img = np.zeros((480, 640, 3), dtype='uint8')
+#img = np.zeros((1164, 640, 3), dtype='uint8') #OJ
+
 #Loading Haar Cascade in this case
 path1 = '/home/oj/oj_scripts/archive/oj_stop.xml'
 haar_face_cascade =cv.CascadeClassifier(path1)
-
-end1 = time.time();print('checkpoint1 (ms)',round(1000*(end1 - start), 1))
 
 # gets data packets from zmq via port 8002
 context = zmq.Context()
 frame = context.socket(zmq.SUB)
 frame.connect("tcp://127.0.0.1:%d" % (service_list['frame'].port))  # port 8002
 frame.setsockopt(zmq.SUBSCRIBE, "")
-
-end2 = time.time();print('checkpoint2 (ms)',round(1000*(end1 - start), 1))
-end2 = time.time();print('checkpoint2 (ms)',round(1000*(end1 - start), 1))
 # frame Display
 while 1:
 
@@ -48,8 +61,3 @@ while 1:
       end6 = time.time();print('checkpoint6 (ms)', round(1000 * (end1 - start), 1))
   cv.imshow('car"s full frame', im)
   cv.waitKey(1)
-
-# Displaying the rectangle over image
-  #plt.imshow(cv.cvtColor(im, cv.COLOR_BGR2RGB))
-  #plt.show()
-  #cv.imshow('cars full frame', im)
