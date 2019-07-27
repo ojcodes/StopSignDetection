4b62c7ee0b6ff741ac9d30124fadafc9be80771b
Osama Javaid <31596193+ojcodes@users.noreply.github.com>
2019-07-27 17:15:51 -0400

    Add files via upload

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,7 @@
 # This script detects stop signs based on haar cascade on a direct video file
 import time
 import cv2 as cv
+import numpy as np
 
 start = time.time()
 # Loading Haar Cascade in this case
@@ -8,22 +9,48 @@ path1 = '/home/oj/oj_scripts/archive/oj_stop.xml'
 haar_face_cascade = cv.CascadeClassifier(path1)
 
 end1 = time.time();print('checkpoint1 (ms)', round(1000 * (end1 - start), 1))
-cap = cv.VideoCapture('stopv1.hevc')
+cap = cv.VideoCapture('stopvidtrim1.mp4')
 # frame Display
 while 1:
     start = time.time()
     ret, frame = cap.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    #for red sing
+    # converting from BGR to HSV color space
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # Range for lower red
+    lower_red = np.array([0, 100, 120])
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv.inRange(hsv, lower_red, upper_red)
+    """
+    # Range for upper range
+    lower_red = np.array([170, 75, 70])
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv.inRange(hsv, lower_red, upper_red)
+    """
+    # Generating the final mask to detect red color
+    #mask = mask1 + mask2
+    mask=mask1
+    # Bitwise-AND mask and original image
+    res = cv.bitwise_and(frame, frame, mask=mask)
+    im2, contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+
+    cv.imshow('mask', mask)
+    #cv.imshow('res', res)
+
+    # Generating the final
     stops = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4)
     # go over list of stop signs and draw them as rectangles on original colored image
     for (x, y, w, h) in stops:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+      cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+      cv.drawContours(frame, contours, -1, (0, 255, 0), 3)
     cv.imshow('frame', frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(100) & 0xFF == ord('q'):
         break
     end2 = time.time();
     print('checkpoint2 (ms)', round(1000 * (end2 - start), 1))
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
--- stopsignvidv9_v3.py
+++ stopsignvidv9_v3.py
@@ -1,28 +1,41 @@
-# This script detects stop signs based on haar cascade on a direct video file. Smallest code
+# This script detects stop signs based on haar cascade on a direct video file. Also, tries to find the distance of the stop sign
 import time
 import cv2 as cv
+
 
 start = time.time()
 # Loading Haar Cascade in this case
 path1 = '/home/oj/oj_scripts/archive/oj_stop.xml';haar_face_cascade = cv.CascadeClassifier(path1)
+path2 ='/home/oj/oj_scripts/videos/stopvid.mp4'
 
+haar_face_cascade = cv.CascadeClassifier(path1)
 end1 = time.time();print('checkpoint1 (ms)', round(1000 * (end1 - start), 1))
-cap = cv.VideoCapture('stopv1.hevc')
+cap = cv.VideoCapture(path2)
+
+
 # frame Display
 while 1:
     start = time.time()
     ret, frame = cap.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    stops = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    im2, contours, hierarchy = cv.findContours(gray, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    stops = haar_face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
     # go over list of stop signs and draw them as rectangles on original colored image
     for (x, y, w, h) in stops:
-        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv.imshow('frame', frame)
+      cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3) #zoomed in, x starts to go to right and y starts to go up
+      #print("x",x,"w",w,"w-x", w-x,"y",y, "h", h, "y-h", h-y)
+      #print ("h (pixels):", h) #X pixels @ ScaleFActor=1.2 and minNeighbours = 4, rectangle size=2
+      #print ('Calibration - h [pixels]/12[ft]:',h/12) #where X pixels at 12ft is known distance for this 12ft videos.
+      #print "1/(h/11.0)", 1/(h/11.0)
+      dis= round(1*(133/(h/11.3)),1) #h/12 for 12 ft video gives 11, thus using it.
+      print 'distance(ft):',dis
+      #print("The number is " + str(dis))
 
-    if cv.waitKey(10) & 0xFF == ord('q'):
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
-    end2 = time.time();print('checkpoint2 (ms)', round(1000 * (end2 - start), 1))
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
